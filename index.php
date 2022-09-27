ea507055ff78abcf9d0f7cb92c6b23edd8bd7d4f
Sm Ashraful <smashraful.dev@gmail.com>
2022-09-28 03:36:39 +0600

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $packages = array(
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.style.css">
 
 </head>
 
